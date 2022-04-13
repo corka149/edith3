f68a29d7f1a092c362cf1b73022b8df780e0d912
corka149 <corka149@mailbox.org>
2022-04-13 21:51:04 +0200

    Add PaperProvider
    As stated by https://callstack.github.io/react-native-paper/getting-started.html#usage

--- App.tsx
+++ App.tsx
@@ -1,22 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {StatusBar} from 'expo-status-bar';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+
+import {Provider as PaperProvider} from 'react-native-paper';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+    const isLoadingComplete = useCachedResources();
+    const colorScheme = useColorScheme();
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+    if (!isLoadingComplete) {
+        return null;
+    } else {
+        return (
+            <PaperProvider>
+                <SafeAreaProvider>
+                    <Navigation colorScheme={colorScheme}/>
+                    <StatusBar/>
+                </SafeAreaProvider>
+            </PaperProvider>
+        );
+    }
 }
