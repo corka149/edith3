0ce34e2fa95e6b1950fdcff0a9fb5b72b4dbc841
corka149 <corka149@mailbox.org>
2022-04-23 13:28:12 +0200

    Add basic navigation

---
+++ screens/EdithScreen.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import {Text, View} from 'react-native';
+import {DefaultStyle} from "../styles/default";
+
+export default function EdithScreen() {
+    return (
+        <View style={DefaultStyle}>
+            <Text>Edith3</Text>
+        </View>
+    );
+}
---
+++ screens/JarvisScreen.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import {Text, View} from 'react-native';
+import {DefaultStyle} from "../styles/default";
+
+export default function JarvisScreen() {
+    return (
+        <View style={DefaultStyle}>
+            <Text>Jarvis</Text>
+        </View>
+    )
+}
