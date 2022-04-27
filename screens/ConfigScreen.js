d2a91037bd2e7527302ea7e6c16ccbda55224f23
corka149 <corka149@mailbox.org>
2022-04-27 22:16:50 +0200

    Save token via save FAB

--- screens/ConfigScreen.js
+++ screens/ConfigScreen.js
@@ -1,22 +1,30 @@
 import * as React from 'react';
-import {StyleSheet, Text} from 'react-native';
-import {DefaultStyle} from "../styles/default";
-import {SafeAreaView} from 'react-native-safe-area-context';
-import {TextInput, Headline, Divider} from 'react-native-paper';
+import { StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { TextInput, Headline, Text } from 'react-native-paper';
 import * as secureStorage from '../services/secureStorage';
+import { FAB, Snackbar } from 'react-native-paper';
+
 
 export default function ConfigScreen() {
     const [token, setToken] = React.useState("");
 
+    const [visible, setVisible] = React.useState(false);
+
+    const save = async (token) => {
+        await secureStorage.saveToken(token);
+        setVisible(true);
+    }
+
     React.useEffect(async () => {
-       const savedToken = await secureStorage.getToken();
-       setToken(savedToken);
+        const savedToken = await secureStorage.getToken();
+        setToken(savedToken);
     });
 
     return (
-        <SafeAreaView style={[DefaultStyle.default, style.container]}>
-            <Headline>
-                <Text>Config</Text>
+        <SafeAreaView style={style.container}>
+            <Headline style={style.headline}>
+                Config
             </Headline>
             <TextInput
                 label='Token'
@@ -25,16 +33,47 @@ export default function ConfigScreen() {
                 onChangeText={async (text) => {
                     if (text.length > 0) {
                         setToken(text);
-                        await secureStorage.saveToken(text);
                     }
                 }}
             />
+            <FAB
+                style={style.fab}
+                small
+                icon="content-save"
+                onPress={async () => save(token)}
+            />
+            <Snackbar
+                visible={visible}
+                style={style.snackbar}
+                onDismiss={() => setVisible(false)}
+                duration={3000}
+                action={{
+                    label: 'x',
+                    onPress: () => setVisible(false),
+                }}
+            >
+                Saved
+            </Snackbar>
         </SafeAreaView>
     );
 }
 
 const style = StyleSheet.create({
     container: {
+        flex: 1,
         margin: 15,
+        justifyContent: 'flex-start',
     },
+    headline: {
+        textAlign: 'center',
+    },
+    fab: {
+        position: 'absolute',
+        backgroundColor: '#ff9800',
+        margin: 15,
+        right: -10,
+        bottom: -10,
+    },
+    snackbar: {
+    }
 })
